d97651879c3a18d9202841068613bcf9ca26b9e1
Fabian Dario Montoya <37662924+FabianMontoya@users.noreply.github.com>
2020-11-01 14:12:48 -0500

    Update some fix
    Se agregan algunas validaciones para el formulario y algunos mensajes de alerta para informar al usuario mejor de acciones

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var app = new Vue({
     el: '#app',
     data: {
         movies: [
-            {title: 'Star Wars',year: 1975,genre: 'Sci-Fi'},
-            {title: 'Back to the Future',year: 1975,genre: 'Sci-Fi'},
-            {title: 'Rambo - First BLood',year: 1986,genre: 'Action'},
+            {title: 'Star Wars',year: 1975, genre: 'Sci-Fi'},
+            {title: 'Back to the Future',year: 1975, genre: 'Sci-Fi'},
+            {title: 'Rambo - First Blood',year: 1986, genre: 'Action'},
         ],
         model:{
             title: '',
@@ -28,19 +28,36 @@ var app = new Vue({
             }
         },
         saveNew(){
-            this.movies.push(this.model);
-            this.model = {
-                title: '',
-                year: '',
-                genre: ''
-            };
+            if(this.model.title && this.model.genre){
+                let actualYear = new Date().getFullYear();
+                if(this.model.year && this.model.year >= 1895 && this.model.year <= actualYear){
+                    let exist = false;
+                    for(var i in this.movies){
+                        if(this.movies[i].title == this.model.title && this.movies[i].genre == this.model.genre && this.movies[i].year == this.model.year){
+                            exist = true;
+                            break;
+                        }
+                    }
+                    if(!exist){
+                        this.movies.push(this.model);
+                        this.model = {
+                            title: '',
+                            year: '',
+                            genre: ''
+                        };
+                    }else{
+                        alert("No puedes añadir la misma película dos veces, favor validar.");
+                    }
+                }else{
+                    alert("Debes indicar un año valido, recuerda que la fecha debe ser desde el año 1895 y no puede ser mayor al año "+actualYear);
+                }
+            }else{
+                alert("Debes indicar el titulo de la película y su género para poder añadirla a la lista.");
+            }
         },
         deleteMovie(movie){
             let index = this.movies.indexOf(movie)
             this.movies.splice(index, 1)
         }
-    },
-    created() {
-        console.log()
     },
 })
